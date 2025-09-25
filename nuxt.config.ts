5e743f90ba4328c5f17ed18d8d9767dde967a7ab
Muxammad <muxammad@gmail.com>
2025-09-25 18:44:42 +0500

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 export default defineNuxtConfig({
-  
+  css: [
+    "@fortawesome/fontawesome-free/css/all.min.css"
+  ]
   app: {
     head: {
       title: 'Najot Talim - Nuxt',
       
-      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }]
+      meta: [
+        {charset:"utf-8"},
+        { name: 'viewport', 
+          content: 'width=device-width, initial-scale=1' 
+        }
+      ]
     }
   }
   
